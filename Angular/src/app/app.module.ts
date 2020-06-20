0164f49bdab0a761bb501250387b52b10d2ae732
Vitaliy Mamontov <vintixfish@gmail.com>
2020-06-21 00:54:43 +0300

    Routing tuned

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
+import { PromotionService } from './../services/promotion.service';
+import { Promotion } from './../shared/promotion';
+import { DishService } from './../services/dish.service';
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dish: Dish;
+  promotion: Promotion;
+
+  constructor(private dishService: DishService,
+              private promotionService: PromotionService) { }
 
   ngOnInit(): void {
+    this.dish = this.dishService.getFeaturedDish();
+    this.promotion = this.promotionService.getFeaturedPromotion();
   }
 
 }
